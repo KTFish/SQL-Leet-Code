84b61740c7478411b66eb81fe9b56bf47a838bda
Jan Karaś <mygnirut@gmail.com>
2023-07-30 22:24:53 +0200

    Solve problem 1581.

--- leet-code/1581-Customer-Who-Visited-But-Did-Not-Make-Any-Transactions.sql
+++ leet-code/1581-Customer-Who-Visited-But-Did-Not-Make-Any-Transactions.sql
@@ -1,2 +1,14 @@
 -- 1581-Customer-Who-Visited-But-Did-Not-Make-Any-Transactions
 -- Exercise from: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+-- Write a SQL query to find the IDs of the users
+-- who visited without making any transactions and the number of times they made these types of visits.
+SELECT
+    v.customer_id,
+    COUNT(*) AS count_no_trans
+FROM
+    Visits v
+    LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE
+    t.transaction_id IS NULL
+GROUP BY
+    v.customer_id
