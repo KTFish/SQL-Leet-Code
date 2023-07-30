6d20d4e3edf8f29b7999c89b5ed73b0045f04088
Jan Karaś <mygnirut@gmail.com>
2023-07-30 20:48:29 +0200

    Fix SQL_FILE_NAME link problem.

--- create_file_scipt.py
+++ create_file_scipt.py
@@ -1,14 +1,17 @@
 import os
 
-# SQL_FILE_NAME = f"Revising the Select Query I"
+# Fill this out
 SOURCE = "leet-code"  # Hacker Rank or Leet Code
-LINK = "https://leetcode.com/problems/list-the-products-ordered-in-a-period/"
+LINK = r"https://leetcode.com/problems/list-the-products-ordered-in-a-period/"
+
+# Automated file creation...
 SQL_FILE_NAME = LINK.split("/")[4]
 SQL_TITLE_NAME = SQL_FILE_NAME.replace("-", " ").title()
 
-
-PATH = f"{SOURCE}/{SQL_FILE_NAME}.sql"
+PATH = f"./{SOURCE}/{SQL_FILE_NAME}.sql"
 README_PATH = f"{SOURCE}/README.md"
+
+print("PATH: ", PATH)
 
 # Create folder if it dosen't exists
 if not os.path.exists(SOURCE):
@@ -18,14 +21,17 @@ if not os.path.exists(SOURCE):
 if not os.path.exists(PATH):
     with open(PATH, "w") as file:
         # Add a title row
-        file.write("-- Revising the Select Query I")
+        file.write(f"-- {SQL_FILE_NAME}\n")
+        file.write(f"-- Exercise from: {LINK}\n")
 
 # Write information about the problem to a .md file
 if not os.path.exists(README_PATH):
     with open(README_PATH, "w") as readme:
         readme.write(f"# Problems solved - {SOURCE.replace('-',' ').title()}\n\n")
-        readme.write("| Link | Problem Name |\n")
-        readme.write("| --- | --- |\n")
+        readme.write("| Link | Problem Name | My solution |\n")
+        readme.write("| --- | --- | --- |\n")
 
 with open(README_PATH, "a") as readme:
-    readme.write(f"| [Link]({LINK}) | {SQL_TITLE_NAME} |\n")
+    readme.write(
+        f"| [Link]({LINK}) | {SQL_TITLE_NAME} | [My solution]({SQL_FILE_NAME}.sql) |\n"
+    )
