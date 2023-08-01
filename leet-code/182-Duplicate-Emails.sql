2f6c48aa5f751a2045156d15b2fd7452eef4bdb4
Jan Karaś <mygnirut@gmail.com>
2023-08-01 11:25:27 +0200

    Solve problem 182.

--- leet-code/182-Duplicate-Emails.sql
+++ leet-code/182-Duplicate-Emails.sql
@@ -1,2 +1,12 @@
 -- 182-Duplicate-Emails
 -- Exercise from: https://leetcode.com/problems/duplicate-emails/
+SELECT
+    email AS Email
+FROM
+    Person
+WHERE
+    email IS NOT NULL
+GROUP BY
+    email
+HAVING
+    COUNT(email) >= 2
