8cafd07ed9af5a1b4db47d4661e418f07bae361b
Jan Karaś <mygnirut@gmail.com>
2023-08-02 06:25:04 +0200

    Solve poblem 1789.

---
+++ leet-code/1789-Primary-Department-For-Each-Employee.sql
@@ -0,0 +1,19 @@
+-- 1789-Primary-Department-For-Each-Employee
+-- Exercise from: https://leetcode.com/problems/primary-department-for-each-employee/
+SELECT
+    employee_id,
+    department_id
+FROM
+    Employee
+WHERE
+    primary_flag = 'Y'
+    OR employee_id IN (
+        SELECT
+            employee_id
+        FROM
+            Employee
+        GROUP BY
+            employee_id
+        HAVING
+            COUNT(employee_id) = 1
+    );
